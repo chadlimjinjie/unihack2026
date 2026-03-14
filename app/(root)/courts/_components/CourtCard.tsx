39f748cac89c03889ef4bf09df587d4702c1a217
Chad Lim <chadlimjinjie@gmail.com>
2026-03-14 15:33:56 +1100

    Refactor courts page and add court card component

---
+++ app/(root)/courts/_components/CourtCard.tsx
@@ -0,0 +1,44 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+
+export default function CourtCard({
+    id,
+    image,
+    name,
+    location,
+}: {
+    id: bigint
+    image: string | null
+    name: string | null
+    location: string | null
+}) {
+    return (
+        <Card className="relative w-full max-w-sm pt-0">
+            {/* <div className="absolute inset-0 z-30 aspect-video bg-black/35" /> */}
+            <img
+                src={image ?? "https://avatar.vercel.sh/shadcn1"}
+                alt="Court cover"
+                className="relative z-20 aspect-video w-full object-cover grayscale"
+                // className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+            />
+            <CardHeader>
+                <CardAction>
+                    <Badge variant="secondary">Featured</Badge>
+                </CardAction>
+                <CardTitle>{name}</CardTitle>
+                <CardDescription>
+                    {location}
+                </CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <Button className="w-full" asChild>
+                    <Link href={`/courts/${id}`}>
+                        View Court
+                    </Link>
+                </Button>
+            </CardFooter>
+        </Card>
+    )
+}
--- app/(root)/courts/page.tsx
+++ app/(root)/courts/page.tsx
@@ -10,6 +10,7 @@ import {
     TabsTrigger,
 } from "@/components/ui/tabs"
 import Link from "next/link"
+import CourtCard from "./_components/CourtCard"
 
 export default async function Page() {
 
@@ -25,35 +26,7 @@ export default async function Page() {
                 <TabsContent value="list">
                     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
                         <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
-                            {courts.map(court => {
-                                return (
-                                    <Card key={court.id} className="relative w-full max-w-sm pt-0">
-                                        <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
-                                        <img
-                                            src={court.image ?? "https://avatar.vercel.sh/shadcn1"}
-
-                                            alt="Court cover"
-                                            className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
-                                        />
-                                        <CardHeader>
-                                            <CardAction>
-                                                <Badge variant="secondary">Featured</Badge>
-                                            </CardAction>
-                                            <CardTitle>{court.name}</CardTitle>
-                                            <CardDescription>
-                                                {court.location}
-                                            </CardDescription>
-                                        </CardHeader>
-                                        <CardFooter>
-                                            <Button className="w-full" asChild>
-                                                <Link href={`/courts/${court.id}`}>
-                                                    View Court
-                                                </Link>
-                                            </Button>
-                                        </CardFooter>
-                                    </Card>
-                                )
-                            })}
+                            {courts.map(court => <CourtCard key={court.id} id={court.id} image={court.image} name={court.name} location={court.location} />)}
                         </div>
                     </div>
 
