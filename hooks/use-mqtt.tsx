3961d2b3092f1514f3ec3b25e7658776fd720bae
Chad Lim <chadlimjinjie@gmail.com>
2026-03-14 21:59:58 +1100

    modified:   app/(root)/courts/[id]/_components/CourtPage.tsx 	new file:   app/(root)/courts/[id]/_components/LiveCount.tsx 	new file:   components/mqtt/MqttProvider.tsx 	new file:   hooks/use-mqtt.tsx 	modified:   package-lock.json 	modified:   package.json

---
+++ hooks/use-mqtt.tsx
@@ -0,0 +1,29 @@
+import { MqttClient } from "mqtt";
+import { useEffect } from "react";
+
+export function useMQTTSubscribe(client: MqttClient | null, topic: string, onMessage: any) {
+    useEffect(() => {
+        if (!client || !client.connected) return;
+        const handleMsg = (receivedTopic: string, message: Buffer) => {
+            console.log("Received message on topic %s: %s", receivedTopic, message.toString());
+            if (receivedTopic === topic) {
+                onMessage(JSON.parse(message.toString()));
+            }
+        };
+        client.subscribe(topic);
+        client.on('message', handleMsg);
+        return () => {
+            client.unsubscribe(topic);
+            client.off('message', handleMsg);
+        };
+    }, [client, topic, onMessage]);
+}
+
+export function useMQTTPublish(client: MqttClient | null) {
+    const publish = (topic: string, message: any, options = {}) => {
+        if (client && client.connected) {
+            client.publish(topic, message, options);
+        }
+    };
+    return publish;
+}
